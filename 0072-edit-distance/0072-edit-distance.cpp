b5db61de7bb377b7eeda49a19ff891500fd33225
Mohit <118717989+Mohitsinghparmarg@users.noreply.github.com>
2024-09-23 23:15:20 +0530

    Time: 9 ms (50.86%), Space: 12.9 MB (41.93%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-
-    int m,n;
-    int t[501][501];
-
-    int solve(string& word1, string& word2,int m,int n){
-             
-            if( m == 0 || n == 0){
-                   return m + n;
-            }
-            if(t[m][n] != -1){
-                 return t[m][n];
-            }
-            if(word1[m-1] == word2[n-1]){ 
-                     return t[m][n] =  solve(word1,word2,m-1,n-1);
-            }
-           else{
-                   int insertChar  = 1 + solve(word1,word2,m,n-1);
-                   int deleteChar  = 1 + solve(word1,word2,m-1,n);
-                   int replaceChar = 1 + solve(word1,word2,m-1,n-1);
-                 return t[m][n] =  min(min(insertChar,deleteChar),replaceChar);
-            }
-        return -1;
-    }
-
     int minDistance(string word1, string word2) {
           
-          m = word1.size();
-          n = word2.size();
-          memset(t,-1,sizeof(t));
-         return solve(word1,word2,m,n);
+        int m = word1.size();
+        int n = word2.size();
+        vector<vector<int>> t(m+1,vector<int>(n+1));
+
+        for(int i = 0; i <= m; i++) {
+            for(int j = 0; j <= n; j++) {
+                if(i == 0 || j == 0) {
+                    t[i][j] = i + j;
+                }
+                else if(word1[i-1] == word2[j-1]) {
+                    t[i][j] = t[i-1][j-1];
+                }
+                else {
+                    t[i][j] = 1 + min(min(t[i][j-1], t[i-1][j]), t[i-1][j-1]);  
+                }
+            }
+        }
+        return t[m][n];
     }
 };
