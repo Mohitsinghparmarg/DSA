53c31fbf2271bbc26fef3218f8f373280bf79ca8
Mohit <118717989+Mohitsinghparmarg@users.noreply.github.com>
2024-09-24 10:30:44 +0530

    Time: 0 ms (100%), Space: 7.9 MB (84.4%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-      
-      int t[101];
-   int solve(string& s, int i,int& n){
-          
-           if(t[i] != -1){
-                return t[i];
-           }
-           if(i == n){
-                 return t[i] = 1; 
-           }
-           if(s[i] == '0'){
-                 return t[i] = 0;
-           }
-
-           int ans = solve(s,i+1,n);
-
-         if(i+1 < n){
-            if( s[i] == '1' || (s[i] == '2' && s[i+1] <= '6')){
-                  ans += solve(s,i+2,n);
-           }
-         }
-        return t[i] =  ans;
-    }
     int numDecodings(string s) {
-          
-          int n = s.size();
-          memset(t,-1,sizeof(t));
-          return solve(s,0,n);
+           
+           int n = s.size();
+           vector<int>t(n+1,0);
+
+           t[n] = 1;
+
+           for(int i = n-1; i>= 0; i--){
+                if(s[i] == '0'){
+                      t[i] = 0;
+                 }
+                else{
+                      t[i] = t[i+1]; // it is the the solve(i+1)...
+                    if(i+1 < n){
+                         if(s[i] == '1' || (s[i] == '2' && s[i+1] <= '6')){
+                                 t[i] += t[i+2];     // it is solve(i+2)...
+                    }
+                  }
+                }
+           }
+        return t[0];
     }
 };
