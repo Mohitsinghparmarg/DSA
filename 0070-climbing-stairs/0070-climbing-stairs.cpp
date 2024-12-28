83d33823b2c927d63d6c5b8af099773aaa4461ff
Mohit Singh Parmar <118717989+Mohitsinghparmarg@users.noreply.github.com>
2024-12-28 12:16:21 +0530

    Time: 0 ms (100%), Space: 7.7 MB (48.29%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int climbStairs(int n) {
-          
-          int t[n+1];
-          memset(t,-1,sizeof(t));
-          if(n<=2) return n;
-          t[0] = 0;
-          t[1] = 1;
-          t[2] = 2;
-        for(int i = 3; i<n+1; i++){
-             
-             t[i] = t[i-1] + t[i-2];
+     
+        if (n <= 2) return n;
+        
+        int t[n + 1];  
+        t[0] = 1;      
+        t[1] = 1;      
+        t[2] = 2;      
+
+        
+        for (int i = 3; i <= n; i++) {
+            t[i] = t[i - 1] + t[i - 2];
         }
-        return t[n];
-          
+        
+        return t[n];  
     }
 };
