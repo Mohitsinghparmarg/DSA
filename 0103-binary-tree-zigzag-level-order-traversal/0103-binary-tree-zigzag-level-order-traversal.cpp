bea4e19978591ce4724e9afeef3f39fb8865eb99
Mohit Singh Parmar <118717989+Mohitsinghparmarg@users.noreply.github.com>
2025-04-08 03:25:50 +0530

    Time: 0 ms (100%), Space: 15 MB (77.05%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -6,33 +6,39 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-           
-           vector<vector<int>>ans;
-          if(root == NULL)
-                return ans;
-          queue<TreeNode*>q;
-          q.push(root);
-          bool leftToRight = true;
-         while(!q.empty()){
-               int size = q.size();
-               vector<int>v(size);
-             for(int i = 0; i<size; i++){
-                  TreeNode* node = q.front();
-                  q.pop();
-                int j = leftToRight ? i : size - i -1;
-                 if(node->left) q.push(node->left);
-                 if(node->right) q.push(node->right);
-                 v[j] = node->val;
-             }
-             leftToRight = !leftToRight;
-             ans.push_back(v);
-         }
-         return ans;
+
+        vector<vector<int>> ans;
+        if (root == NULL) {
+            return ans;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        bool flag = true;
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> level(size);
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (node->left != NULL) {
+                    q.push(node->left);
+                }
+                if (node->right != NULL) {
+                    q.push(node->right);
+                }
+                int x = flag ? i : size - i - 1;
+                level[x] = node->val;
+            }
+            flag = !flag;
+            ans.push_back(level);
+        }
+        return ans;
     }
 };
