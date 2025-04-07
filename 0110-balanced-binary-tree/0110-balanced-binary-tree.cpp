a1e5658486ca3a08b3d285f005477cc6e06a00d9
Mohit Singh Parmar <118717989+Mohitsinghparmarg@users.noreply.github.com>
2025-04-08 02:39:15 +0530

    Time: 0 ms (100%), Space: 23.1 MB (24.14%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -6,30 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
+    int Solve(TreeNode* root) {
 
-   int Solve(TreeNode* &root){
-             
-             if(root == NULL)
-                return 0;
-            int leftPart = Solve(root->left);
-            if(leftPart == -1)
-                  return -1;
-            int rightPart = Solve(root->right);
-            if(rightPart == -1)
-                  return -1;
-            if(abs(leftPart - rightPart) > 1)
-                   return -1;
-         return max(leftPart,rightPart) + 1;
-   }
-
-
-    bool isBalanced(TreeNode* root) {
-             
-             return Solve(root) != -1;
+        if (root == NULL) {
+            return 0;
+        }
+        int leftHeight = Solve(root->left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+        int rightHeight = Solve(root->right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+        if (abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return 1 + max(leftHeight, rightHeight);
     }
+
+    bool isBalanced(TreeNode* root) { return Solve(root) != -1; }
 };
