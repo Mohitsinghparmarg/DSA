102b4c41893425013bf365854c8720a81fdeef49
Mohit Singh Parmar <118717989+Mohitsinghparmarg@users.noreply.github.com>
2025-04-08 21:34:47 +0530

    Time: 1 ms (59.06%), Space: 16.4 MB (31.9%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -6,49 +6,39 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-       
-        map<int, map<int, multiset<int>>> mpp;
 
-       
+        map<int, map<int, multiset<int>>> mpp;
         queue<pair<TreeNode*, pair<int, int>>> q;
         q.push({root, {0, 0}});
-
         while (!q.empty()) {
             auto p = q.front();
             q.pop();
-
-            TreeNode* temp = p.first;
+            TreeNode* node = p.first;
             int x = p.second.first;
             int y = p.second.second;
-
-          
-            mpp[x][y].insert(temp->val);
-
-         
-            if (temp->left) {
-                q.push({temp->left, {x - 1, y + 1}});
+            mpp[x][y].insert(node->val);
+            if (node->left != NULL) {
+                q.push({node->left, {x - 1, y + 1}});
             }
-            if (temp->right) {
-                q.push({temp->right, {x + 1, y + 1}});
+            if (node->right != NULL) {
+                q.push({node->right, {x + 1, y + 1}});
             }
         }
-
-     
         vector<vector<int>> ans;
-        for (auto p : mpp) {
-            vector<int> res;
-            for (auto r : p.second) {
-                res.insert(res.end(), r.second.begin(), r.second.end());
+        for (auto it : mpp) {
+            vector<int> temp;
+            for (auto x : it.second) {
+                temp.insert(temp.end(), x.second.begin(), x.second.end());
             }
-            ans.push_back(res);
+            ans.push_back(temp);
         }
-
         return ans;
     }
 };
