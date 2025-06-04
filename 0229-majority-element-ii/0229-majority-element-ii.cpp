7e3f64afcc8a640a1142720afe2ebb1c516ee673
Mohit Singh Parmar <118717989+Mohitsinghparmarg@users.noreply.github.com>
2025-06-04 18:28:35 +0530

    Time: 0 ms (100%), Space: 23.5 MB (57.84%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,17 +1,43 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-                 
-                 int n = nums.size();
-                unordered_map<int,int>mpp;
-                vector<int>v;
-              for(auto it : nums){
-                    mpp[it]++;
-              }
-              for(auto it : mpp){
-                   if(it.second > n/3)
-                          v.push_back(it.first);
-              }
-            return v;
+
+        int n = nums.size();
+        int count1 = 0;
+        int element1 = INT_MIN;
+        int count2 = 0;
+        int element2 = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            if (count1 == 0 && element2 != nums[i]) {
+                count1++;
+                element1 = nums[i];
+            } else if (count2 == 0 && element1 != nums[i]) {
+                count2++;
+                element2 = nums[i];
+            } else if (element1 == nums[i]) {
+                count1++;
+            } else if (element2 == nums[i]) {
+                count2++;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        vector<int> v;
+        count1 = 0;
+        count2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (element1 == nums[i]) {
+                count1++;
+            } else if (element2 == nums[i]) {
+                count2++;
+            }
+        }
+        int mini = int(n / 3) + 1;
+        if (count1 >= mini)
+            v.push_back(element1);
+        if (count2 >= mini)
+            v.push_back(element2);
+        return v;
     }
 };
