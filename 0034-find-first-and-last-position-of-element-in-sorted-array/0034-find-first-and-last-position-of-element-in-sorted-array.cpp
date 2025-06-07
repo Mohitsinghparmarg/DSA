9e0a71a04a67e74be27399816fa631269cdfedb5
Mohit Singh Parmar <118717989+Mohitsinghparmarg@users.noreply.github.com>
2025-06-07 15:39:12 +0530

    Time: 0 ms (100%), Space: 17.4 MB (86.23%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,53 +1,30 @@
 class Solution {
 public:
+    int Solve(vector<int>& nums, int target, bool flag) {
+        int low = 0;
+        int high = nums.size() - 1;
+        int ans = -1;
+        while (low <= high) {
+            int mid = (low + (high - low) / 2);
+            if (nums[mid] == target) {
+                ans = mid;
+                if (flag)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
 
-    int FirstPositionIs(vector<int>&nums,int target){
-              
-             int start = 0;
-             int end = nums.size() - 1;
-             int FirstPosition = -1;
-            while(start <= end){
-                   int mid = (start + (end - start)/2);
-                 if(nums[mid] == target){
-                       FirstPosition = mid;
-                       end = mid - 1;
-                  }
-                 else if(nums[mid] < target){
-                         start = mid + 1;
-                 }
-                else{
-                      end = mid - 1;
-                }
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
-        return FirstPosition;
+        }
+        return ans;
     }
-
-    int LastPositionIs(vector<int>&nums,int target){
-               
-                int start = 0;
-                int end   = nums.size() - 1;
-                int LastPosition = -1;
-             while(start <= end){
-                  int mid = (start + (end - start)/2);
-                if(nums[mid] == target){
-                       LastPosition = mid;
-                       start = mid + 1;
-                }
-               else if(nums[mid] < target){
-                       start = mid + 1;
-                }
-               else{
-                       end = mid - 1;
-               }
-             } 
-         return LastPosition;
-    }
-
     vector<int> searchRange(vector<int>& nums, int target) {
-          
-          int FirstPosition = FirstPositionIs(nums,target);
-          int LastPosition  = LastPositionIs(nums,target);
-        return {FirstPosition,LastPosition};
-        
+
+        int FirstOccurrence = Solve(nums, target, true);
+        int LastOccurrence = Solve(nums, target, false);
+        return {FirstOccurrence, LastOccurrence};
     }
 };
