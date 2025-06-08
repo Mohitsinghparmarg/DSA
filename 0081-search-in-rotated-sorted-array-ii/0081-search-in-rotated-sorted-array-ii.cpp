eb505b11b669e63e074385d39baf7283c668f4b0
Mohit Singh Parmar <118717989+Mohitsinghparmarg@users.noreply.github.com>
2025-06-08 10:54:50 +0530

    Time: 0 ms (100%), Space: 18 MB (20.08%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-         
-         int start = 0;
-         int end   = nums.size() - 1;
-        while(start <= end){
-             
-             int mid = (start + (end - start)/2);
 
-             
-
-             if(nums[mid] == target){
-                  return true;
-             }
-             if(nums[start] == nums[mid] && nums[mid] == nums[end]){
-                   start++;
-                   end--;
-                   continue;
-             }
-             else if(nums[start] <= nums[mid]){
-                  
-                  if(nums[start] <= target && target <= nums[mid]){
-                        end = mid - 1;
-                  }
-                  else{
-                      start = mid +1;
-                  }
-             }
-             else{
-                  if(nums[mid] <= target && target <= nums[end]){
-                        start = mid + 1;
-                  }
-                  else{
-                       end = mid - 1;
-                  }
-             }
+        int low = 0;
+        int high = nums.size() - 1;
+        while (low <= high) {
+            int mid = (low + (high - low) / 2);
+            if (nums[mid] == target)
+                return true;
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                   low++;
+                   high--;
+            }
+            else if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
         }
         return false;
     }
