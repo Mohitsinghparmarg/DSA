f0bb9d9c1b57e0c591a787b40415880fab610e03
Mohit Jay Prakash <118717989+Mohitsinghparmarg@users.noreply.github.com>
2025-06-14 12:01:22 +0530

    Time: 6 ms (26.21%), Space: 9.8 MB (48.93%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,18 +2,8 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
 
-        unordered_map<char, int> mpp;
-        if (s.size() != t.size())
-            return false;
-        for (auto it : s) {
-            mpp[it]++;
-        }
-        for (auto it : t) {
-            if (mpp.find(it) == mpp.end() || mpp[it] == 0) {
-                return false;
-            }
-            mpp[it]--;
-        }
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
     }
 };
