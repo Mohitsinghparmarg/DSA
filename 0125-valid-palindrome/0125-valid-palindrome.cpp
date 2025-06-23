e51bc83601718598384093552cde8c1efd74bebb
Mohit <118717989+Mohitsinghparmarg@users.noreply.github.com>
2025-06-23 15:40:08 +0530

    Time: 6 ms (8.08%), Space: 10.4 MB (18.12%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str;
+        for (char c : s) {
+            if (isalnum(c))
+                str.push_back(tolower(c));
+        }
+        int l = 0, r = str.size() - 1;
+        while (l < r) {
+            if (str[l++] != str[r--])
+                return false;
+        }
+        return true;
+    }
+};
