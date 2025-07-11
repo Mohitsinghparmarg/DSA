00b886476df632ef661fcc9156b9ae591fa326d1
Mohit <118717989+Mohitsinghparmarg@users.noreply.github.com>
2025-07-11 21:36:00 +0530

    Time: 51 ms (74.27%), Space: 79.4 MB (79.13%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<vector<vector<int>>> dp(
-            prices.size() + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+        vector<vector<int>> after(2, vector<int>(3, 0));
+        vector<vector<int>> current(2, vector<int>(3, 0));
         for (int index = prices.size() - 1; index >= 0; index--) {
             for (int buy = 0; buy <= 1; buy++) {
                 for (int capacity = 1; capacity <= 2; capacity++) {
                     if (buy) {
-                        dp[index][buy][capacity] =
-                            max(-prices[index] + dp[index + 1][0][capacity],
-                                0 + dp[index + 1][1][capacity]);
+                        current[buy][capacity] =
+                            max(-prices[index] + after[0][capacity],
+                                0 + after[1][capacity]);
                     } else {
-                        dp[index][buy][capacity] =
-                            max(prices[index] + dp[index + 1][1][capacity - 1],
-                                0 + dp[index + 1][0][capacity]);
+                        current[buy][capacity] =
+                            max(prices[index] + after[1][capacity - 1],
+                                0 + after[0][capacity]);
                     }
                 }
             }
+            after = current;
         }
-        return dp[0][1][2];
+        return after[1][2];
     }
 };
