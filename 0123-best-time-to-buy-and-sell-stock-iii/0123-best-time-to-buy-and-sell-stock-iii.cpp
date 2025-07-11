c17d20fb3d918a5db2e34c85d21e9830f892af75
Mohit <118717989+Mohitsinghparmarg@users.noreply.github.com>
2025-07-11 21:29:09 +0530

    Time: 400 ms (46.99%), Space: 211.6 MB (40.51%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    int Solve(int index, int buy, int capacity, vector<int>& prices,
-              vector<vector<vector<int>>>& dp) {
-        if (capacity == 0 || index == prices.size()) {
-            return 0;
-        }
-        if (dp[index][buy][capacity] != -1) {
-            return dp[index][buy][capacity];
-        }
-        if (buy) {
-            return dp[index][buy][capacity] =
-                       max(-prices[index] +
-                               Solve(index + 1, 0, capacity, prices, dp),
-                           0 + Solve(index + 1, 1, capacity, prices, dp));
-        } else {
-            return dp[index][buy][capacity] =
-                       max(prices[index] +
-                               Solve(index + 1, 1, capacity - 1, prices, dp),
-                           0 + Solve(index + 1, 0, capacity, prices, dp));
-        }
-    }
-
     int maxProfit(vector<int>& prices) {
         vector<vector<vector<int>>> dp(
-            prices.size(), vector<vector<int>>(2, vector<int>(3, -1)));
-        return Solve(0, 1, 2, prices, dp);
+            prices.size() + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+        for (int index = prices.size() - 1; index >= 0; index--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int capacity = 1; capacity <= 2; capacity++) {
+                    if (buy) {
+                        dp[index][buy][capacity] =
+                            max(-prices[index] + dp[index + 1][0][capacity],
+                                0 + dp[index + 1][1][capacity]);
+                    } else {
+                        dp[index][buy][capacity] =
+                            max(prices[index] + dp[index + 1][1][capacity - 1],
+                                0 + dp[index + 1][0][capacity]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
