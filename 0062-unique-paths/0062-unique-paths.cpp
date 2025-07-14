92747b80d1e935b851b0efe50cd435a018f8ece9
Mohit <118717989+Mohitsinghparmarg@users.noreply.github.com>
2025-07-14 16:02:25 +0530

    Time: 0 ms (100%), Space: 9.4 MB (33.71%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,24 +1,19 @@
- // Company Tags : Amazon, Cisco, Paytm, OLA Cabs, Walmart, LinkedIn
-
-
 class Solution {
 public:
-
-    int t[101][101];
-
-   int solve(int i,int j, int m,int n){
-            if(i == m-1 && j == n-1) return 1;
-            if( i>=m || j>=n) return 0;
-            if(t[i][j] != -1)
-                 return t[i][j];
-          int right = solve(i,j+1,m,n);
-          int down = solve(i + 1,j,m,n);
-        return t[i][j] = right + down; 
-   }
-
+    int Solve(int i, int j, vector<vector<int>>& dp) {
+        if (i == 0 && j == 0) {
+            return 1;
+        }
+        if (i < 0 || j < 0) {
+            return 0;
+        }
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        return dp[i][j] = Solve(i - 1, j, dp) + Solve(i, j - 1, dp);
+    }
     int uniquePaths(int m, int n) {
-           
-           memset(t,-1,sizeof(t));
-           return solve(0,0,m,n);
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        return Solve(m - 1, n - 1, dp);
     }
 };
